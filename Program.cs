0403659e95eb1a768d106f22fb4e005af67b9234
Anton Gadyatskiy <aoagadbart90@gmail.com>
2022-12-16 01:03:23 +0300

    Написал первую программу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+int min = secondNumber;
+
+if(firstNumber>secondNumber) {
+    Console.WriteLine($"{firstNumber} больше"); 
+}
+else {
+    Console.WriteLine($"{secondNumber} больше"); 
+}
